106c8b9b201f06059d1f1ba1295b3a9790b95b7d
Mark Standen <markstanden80@icloud.com>
2020-05-14 22:23:34 +0100

    end of week 2, just cleaning up directories

--- one_month/sum.py
+++
@@ -1,9 +0,0 @@
-def sum(num1,num2):
-
-     return[num1 + num2 , num1 * num2]
-
-print( 'Sum = ', sum(2,4)[0], 'and Product = ', sum(2,4)[1] )
-
-#How can i print the statement with string like
-
-#Sum = 6 and Product = 8
