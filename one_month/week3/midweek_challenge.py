db094f940b2b22968cc890e3e458eb8fced356d3
Mark Standen <markstanden80@icloud.com>
2020-05-15 23:58:18 +0100

    midweek challege complete

--- one_month/week3/midweek_challenge.py
+++ one_month/week3/midweek_challenge.py
@@ -1,7 +1,17 @@
-import pandas as pd
+import pandas
 
-names = ['id', 'name', 'year', 'rating', 'votes', 'length', 'genre']
-data = pd.read_csv('imdb_top_10000.txt', sep='\t', names=names, index_col=0)
+names = [
+         'id', 'name',
+         'year', 'rating',
+         'votes', 'length',
+         'genre'
+         ]
+
+data = pandas.read_csv('imdb_top_10000.txt',
+                       sep='\t',
+                       names=names,
+                       index_col=0
+                       )
 
 question = []
 answer = []
@@ -11,7 +21,7 @@ answer = []
 
 question.append("What was the highest scoring movie in 1996?")
 
-# Returns film ids and years
+# Returns film IDs and years
 # data['year']
 
 # Returns boolean result for whether data['year'] is 1996
@@ -24,7 +34,7 @@ unsorted_year = data[data['year'] == 1996]
 sorted_year = unsorted_year.sort_values(by='rating', ascending=False)
 
 # Assigns the top result only to variable
-top_result = sorted_year.head(1)
+top_result = sorted_year[sorted_year['rating'] == sorted_year['rating'].max()]
 
 # Convert answer to string, without any indexing or column headings
 answer.append(top_result['name'].to_string(header=False, index=False))
@@ -35,28 +45,58 @@ answer.append(top_result['name'].to_string(header=False, index=False))
 question.append("In what year was the highest rated movie of all time made?")
 
 # Sort the data by ratings, highest at the top, take the highest value
-highest_rating = data.sort_values(by='rating', ascending=False).head(1)
+sorted_by_rating = data.sort_values(by='rating', ascending=False)
 
-# Get the value for year
-highest_rating_year = highest_rating['year']
-highest_rating_year.to_string(index=False, header=False)
 
-# Add to answer list, as a string, without index or header
+highest_rating = sorted_by_rating[sorted_by_rating['rating']
+                                  == sorted_by_rating['rating'].max()]
+
+# We just need the year, but name contains year, and looks clearer to me.
+highest_rating_year = highest_rating['name']
+
+# Add to answer list, as a string, without header
+# Left index to clarify the two films
 answer.append(highest_rating_year.to_string(index=False, header=False))
 
 
 # Question Three ############################################
 
 question.append("What five movies have the most votes ever?")
-answer.append(" ")
+
+# Sort by votes, highest on top
+most_votes = data.sort_values(by='votes', ascending=False)
+
+# we need only the top 5 names
+five_movies = most_votes['name'].head(5)
+
+# Convert to string without index or header, and add to list.
+answer.append(five_movies.to_string(index=False, header=False))
 
 
 # Question Four #############################################
 
 question.append("What year in the 1960s had the highest average movie rating?")
-answer.append(" ")
+
+# get films from the sixties only
+sixties_only = data[(1959 < data['year'])
+                    & (data['year'] < 1970)]
+
+# Group films by year, average the rating
+average_by_year = sixties_only.groupby('year')['rating'].mean()
+
+# Sort by average rating, take top result
+best_year = average_by_year.sort_values(ascending=False).head(1)
+
+# Year is the index value in this set
+year_and_rating = best_year.to_string(index=True, header=False)
+
+# Split the answer into a list
+year = year_and_rating.split(' ', 1)
+
+# First entry is year
+answer.append(year[0])
 
 for question_number in range(1, len(question)+1):
-    print(f"Question Number : {question_number}")
+    print(f"\nQuestion Number : {question_number}")
     print(question.pop(0))
     print(answer.pop(0), '\n')
